4f577c98ab078d4cd1a5142c1f462864c9e5f3ed
dixon <dixon2678@gmail.com>
2022-05-17 00:48:14 +0800

    Altered for Cloud Run deployment

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM python:3.8.2
-WORKDIR /
+
+ENV PYTHONBUFFERED True
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
 RUN apt-get update
 RUN apt-get install default-jdk -y
 
 COPY ./requirements.txt /requirements.txt
-RUN pip install -r requirements.txt
 
-COPY . /
+RUN pip install -r requirements.txt
 
 ENTRYPOINT [ "python" ]
 
-CMD ["./src/main.py"]
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- src/main.py
+++ src/main.py
@@ -4,11 +4,19 @@ import os
 from functions import extractLoad
 from datetime import datetime
 from google.cloud import bigquery
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def el_job():
+    el = extractLoad()
+    df = el.fetch_api()
+    final_df = el.add_datetime(df)
+    return "Job complete"
+    #el.load_bigquery(final_df)
 
 # Main
-
-el = extractLoad()
-df = el.fetch_api()
-final_df = el.add_datetime(df)
-el.load_bigquery(final_df)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+ 
